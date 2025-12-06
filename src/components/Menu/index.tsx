179c55c199c38ef1895bc4130b0983449f3b8794
guilhermehfr <guihenrique.bra@gmail.com>
2025-12-06 17:00:33 -0400

    feat: cria hook para controlar tema da pagina

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,28 +1,65 @@
 import styles from './styles.module.css';
 import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { useState } from 'react';
+
+type AvaiableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvaiableThemes>('light');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
   return (
     <>
       <nav className={styles['menu']}>
         <ul>
           <li className={styles['menu-item']}>
-            <a className={styles['menu-link']} href='#'>
+            <a
+              className={styles['menu-link']}
+              href='#'
+              aria-label='Initial page'
+              title='Initial page'
+            >
               <HouseIcon />
             </a>
           </li>
           <li>
-            <a className={styles['menu-link']} href='#'>
+            <a
+              className={styles['menu-link']}
+              href='#'
+              aria-label='History'
+              title='Tasks history'
+            >
               <HistoryIcon />
             </a>
           </li>
           <li>
-            <a className={styles['menu-link']} href='#'>
+            <a
+              className={styles['menu-link']}
+              href='#'
+              aria-label='Configurations'
+              title='Configurations'
+            >
               <SettingsIcon />
             </a>
           </li>
           <li>
-            <a className={styles['menu-link']} href='#'>
+            <a
+              className={styles['menu-link']}
+              href='#'
+              aria-label={`Change theme to ${theme}`}
+              title={`Change theme to ${theme}`}
+              onClick={handleThemeChange}
+            >
               <SunIcon />
             </a>
           </li>
