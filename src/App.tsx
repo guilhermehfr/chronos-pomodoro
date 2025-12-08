cedf0e0c00ec4743a07c496200924fd6e8a8e24e
guilhermehfr <guihenrique.bra@gmail.com>
2025-12-08 16:53:39 -0400

    feat: implementa pagina Home em App.tsx, adiciona transição leve global

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
-import { Countdown } from './components/Countdown';
-import { FormCycle } from './components/FormCycle';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 import './styles/Normalize.css';
 import './styles/theme.css';
@@ -11,18 +7,7 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-      <Container>
-        <Heading />
-      </Container>
-      <Container>
-        <Countdown />
-      </Container>
-      <Container>
-        <FormCycle />
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
+      <Home />
     </>
   );
 }
