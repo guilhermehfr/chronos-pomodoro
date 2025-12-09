27e6bbfb1e5adfb65c26fd00757973272f1b9804
guilhermehfr <guihenrique.bra@gmail.com>
2025-12-09 15:03:15 -0400

    feat: utiliza componente padrao de paginas nas paginas PomodoTechnique e NotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,20 +1,16 @@
-import { Container } from '../../components/Container';
 import { MainTemplate } from '../../templates/MainTemplate';
-import styles from './styles.module.css';
+import { GenericHTML } from '../../components/GenericHTML';
 
 export function NotFound() {
   return (
     <>
       <MainTemplate>
-        <Container>
-          <div className={styles.wrapper}>
-            <h1 className={styles.title}>Sorry, we couldn't find that page</h1>
-            <p>
-              Try go to <a href='#'>Home</a> or acess your{' '}
-              <a href='#'>History</a>
-            </p>
-          </div>
-        </Container>
+        <GenericHTML>
+          <h1>Sorry, we couldn't find that page</h1>
+          <p>
+            Try go to <a href='#'>Home</a> or acess your <a href='#'>History</a>
+          </p>
+        </GenericHTML>
       </MainTemplate>
     </>
   );
---
+++ src/pages/PomodoroTechnique/index.tsx
@@ -0,0 +1,29 @@
+import { MainTemplate } from '../../templates/MainTemplate';
+
+import { GenericHTML } from '../../components/GenericHTML';
+
+export function PomodoroTechnique() {
+  const highlight = { color: 'var(--primary)' };
+  return (
+    <>
+      <MainTemplate>
+        <GenericHTML>
+          <h1>The Pomodoro Technique &#127813;</h1>
+          <p>
+            The <span style={highlight}> Pomodoro Technique</span> is a time
+            management method using a timer to break work into focused 25-minute
+            intervals, separated by short 5-minute breaks, with longer breaks
+            after four pomodoros.
+          </p>
+          <p>
+            It is designed to boost focus, fight procrastination, and prevent
+            burnout by managing attention in manageable chunks. Created by
+            <span style={highlight}> Francesco Cirillo</span>, who used a
+            tomato-shaped timer, it helps people work with time by creating
+            urgency and ensuring regular rest.
+          </p>
+        </GenericHTML>
+      </MainTemplate>
+    </>
+  );
+}
