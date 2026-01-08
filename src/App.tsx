36fecff0388994b7dc1fcf9a3c6c16b2705ca2a9
guilhermehfr <guihenrique.bra@gmail.com>
2026-01-07 22:02:54 -0400

    feat: add initial state for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
+
+import type { TaskStateModel } from './models/TaskStateModel';
+
 import { Home } from './pages/Home';
 
 import './styles/Normalize.css';
 import './styles/theme.css';
 import './styles/global.css';
-import { useState } from 'react';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  FormattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  const [state, setState] = useState();
+  const [state, setState] = useState(initialState);
 
   return (
     <>
-      <Home />
+      <Home state={state} setState={setState} />
     </>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
 import { MainTemplate } from '../../templates/MainTemplate';
 import { TaskForm } from '../../components/TaskForm';
 
-export function Home() {
+import type { TaskStateModel } from '../../models/TaskStateModel';
+
+interface HomeProps {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+}
+
+export function Home(props: HomeProps) {
   return (
     <>
       <MainTemplate>
