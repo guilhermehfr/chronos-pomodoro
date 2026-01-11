22de515620ee674f51c0d1e90d09c975332f458a
guilhermehfr <guihenrique.bra@gmail.com>
2026-01-11 05:35:50 -0400

    feat: reestructure task context folders

---
+++ src/contexts/TaskContext/TaskContext.tsx
@@ -0,0 +1,13 @@
+import type { TaskStateModel } from "../../models/TaskStateModel";
+import { createContext } from "react";
+import { initialTaskState } from "./initialTaskState";
+
+type TaskContextProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export const TaskContext = createContext<TaskContextProps>({
+  state: initialTaskState,
+  setState: () => {},
+});
---
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import { initialTaskState } from './initialTaskState';
+import { TaskContext } from "./TaskContext";
+
+export function TaskContextProvider({ children }: Record<string, React.ReactNode>) {
+  const [state, setState] = useState(initialTaskState);
+  
+  return (
+    <TaskContext.Provider value={{ state, setState}}>
+      { children }
+    </TaskContext.Provider>
+  );
+};
