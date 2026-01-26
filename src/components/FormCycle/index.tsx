70e0b756a068e1a03aaf068b824d0a2faeb841c3
guilhermehfr <guihenrique.bra@gmail.com>
2026-01-26 02:59:36 -0400

    feat: implement to formCycle component inital task state treatment

--- src/components/FormCycle/index.tsx
+++ src/components/FormCycle/index.tsx
@@ -1,19 +1,56 @@
 import { useRef } from 'react';
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 
+import type { TaskModel } from '../../models/TaskModel';
+
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { InputChronos } from '../InputChronos';
 
 import styles from './styles.module.css';
 
 export function FormCycle() {
   const buttonColor: string = 'red';
+
   const buttonIcon: React.ReactNode =
     buttonColor === 'red' ? <StopCircleIcon /> : <PlayCircleIcon />;
 
   const taskNameInput = useRef<HTMLInputElement>(null);
+  const { setState } = useTaskContext();
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName: string = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa para iniciar');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    setState(prevState => {
+      const secondsRemaining = newTask.duration * 60;
+
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:01',
+      };
+    });
   }
 
   return (
