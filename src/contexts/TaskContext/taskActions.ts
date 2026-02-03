bff60a0abedf64697a4d3bf83f923e8e31fabb48
guilhermehfr <guihenrique.bra@gmail.com>
2026-02-03 04:11:40 -0400

    fix: fix error where types where not being well type-checked by ts

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -1,14 +1,17 @@
 import type { TaskModel } from '../../models/TaskModel';
 
-export const TaskActionTypes = {
+export const TaskActionType = {
   START_TASK: 'START_TASK',
   INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
 } as const;
 
 export type TaskActionTypes =
-  (typeof TaskActionTypes)[keyof typeof TaskActionTypes];
+  (typeof TaskActionType)[keyof typeof TaskActionType];
 
-export type TaskActionModel = {
-  type: TaskActionTypes;
-  payload: TaskModel;
-};
+export type TaskActionModel =
+  | {
+      type: Exclude<TaskActionTypes, typeof TaskActionType.RESET_STATE>;
+      payload: TaskModel;
+    }
+  | { type: typeof TaskActionType.RESET_STATE };
--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -1,15 +1,18 @@
 import type { TaskStateModel } from '../../models/TaskStateModel';
-import { type TaskActionModel, TaskActionTypes } from './taskActions';
+import { type TaskActionModel, TaskActionType } from './taskActions';
 
 export function taskReducer(
   state: TaskStateModel,
   action: TaskActionModel,
 ): TaskStateModel {
   switch (action.type) {
-    case TaskActionTypes.INTERRUPT_TASK: {
+    case TaskActionType.INTERRUPT_TASK: {
       return { ...state };
     }
-    case TaskActionTypes.START_TASK: {
+    case TaskActionType.START_TASK: {
+      return { ...state };
+    }
+    case TaskActionType.RESET_STATE: {
       return { ...state };
     }
   }
