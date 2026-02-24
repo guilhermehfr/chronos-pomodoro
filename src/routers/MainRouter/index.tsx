9d2e2e4115613c3a65ff0fb7611165d78b961231
guilhermehfr <guihenrique.bra@gmail.com>
2026-02-24 03:47:21 -0400

    Refactor: Dynamic route title management

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -1,17 +1,34 @@
 import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+
 import { Home } from '../../pages/Home';
-import { NotFound } from '../../pages/NotFound';
 import { PomodoroTechnique } from '../../pages/PomodoroTechnique';
 import { History } from '../../pages/History';
+import { Settings } from '../../pages/Settings';
+import { NotFound } from '../../pages/NotFound';
 
 function ScrollToTop() {
+  const { state } = useTaskContext();
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+
+    if (state.activeTask || pathname === '/') {
+      document.title = 'Chronos Pomodoro';
+      return;
+    }
+
+    const pathnameFormatted = pathname.replace(
+      /^\/([a-z]+)(?:-([a-z]+))?\/$/, // regex to match paths like /about-pomodoro/ or /history/ and capture the segments
+      (_, p1, p2) =>
+        `${p1.charAt(0).toUpperCase() + p1.slice(1)} ${p2 ? p2.charAt(0).toUpperCase() + p2.slice(1) : ''}`,
+    );
+
+    document.title = `${pathnameFormatted} - Chronos Pomodoro`;
+  }, [pathname, state.activeTask]);
 
   return null;
 }
